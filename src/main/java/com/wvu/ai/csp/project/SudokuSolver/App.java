bcc8429cbee36a3617672276327912a3b6a5d8ed
Shane Hogan <shhogan@mix.wvu.edu>
2016-04-09 15:54:35 -0400

    modified gitignore

--- src/main/java/com/wvu/ai/csp/project/SudokuSolver/App.java
+++ src/main/java/com/wvu/ai/csp/project/SudokuSolver/App.java
@@ -2,13 +2,16 @@ package com.wvu.ai.csp.project.SudokuSolver;
 
 import com.wvu.ai.csp.project.SudokuSolver.Cell;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 /**
  * Created by Shane Hogan on 4/9/16.
  */
 public class App {
     private final static int SIZE = 9;
+    static  Scanner kbd = new Scanner(System.in);
 
     public static void main(String[] args){
         Cell[][] sudokuBoard;
@@ -28,7 +31,6 @@ public class App {
     public static Cell[][] generateUserSudokuBoard(){
         Cell[][] newBoard = new Cell[SIZE][SIZE];
         int cell = 0;
-        Scanner kbd = new Scanner(System.in);
         for(int i = 0; i < SIZE; i++){
             for(int j = 0; j < SIZE; j++){
                 cell = kbd.nextInt();
@@ -37,7 +39,7 @@ public class App {
                     i = 0;
                     j = 0;
                 }
-                newBoard[i][j] = new Cell(cell);
+                newBoard[i][j] = new Cell(cell,String.valueOf((i+1) + "," + (j+1)));
             }
         }
         return newBoard;
@@ -45,8 +47,14 @@ public class App {
 
     public static void printBoard(Cell[][] board){
         for(int i = 0; i < SIZE; i ++){
+            if(i%3==0 && i !=0){
+                System.out.print("\n");
+            }
             for(int j = 0; j < SIZE; j++){
-                System.out.print(board[i][j].getValue() + " ");
+                if(j%3 == 0 && j != 0){
+                    System.out.print("\t");
+                }
+                System.out.print(board[i][j].getName() + " ");
             }
             System.out.print("\n");
         }
@@ -57,4 +65,13 @@ public class App {
 
         return solveable;
     }
+
+    public List<Cell> generateListOfNames(Cell[][] board){
+        List<Cell> nameList = new ArrayList<Cell>();
+        for(int i = 0; i < SIZE; i++){
+            nameList.addAll(Arrays.asList(board[i]).subList(0, SIZE));
+        }
+        return nameList;
+    }
+
 }
