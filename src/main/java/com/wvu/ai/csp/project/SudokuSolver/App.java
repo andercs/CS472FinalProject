b284c9482c83b3203b76e4955c80816bac1777f3
Shane Hogan <shhogan@mix.wvu.edu>
2016-04-09 18:45:07 -0400

    updated invalid number logic

--- src/main/java/com/wvu/ai/csp/project/SudokuSolver/App.java
+++ src/main/java/com/wvu/ai/csp/project/SudokuSolver/App.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
  * Created by Shane Hogan on 4/9/16.
  */
 public class App {
-    private final static int SIZE = 9;
+    //TODO replace size with user input
+    private static int SIZE = 9;
     static  Scanner kbd = new Scanner(System.in);
 
     public static void main(String[] args){
@@ -16,32 +17,36 @@ public class App {
 
 
         //TODO - Ask for board size & check that input is perfect square
-        sudokuBoard = generateUserSudokuBoard();
+
 
         System.out.println("Create your sudoko table by entering the cells. " +
                 "\nEnter the entire top row then hit enter." +
                 "\nEach cell should contain only a single digit between 1-9. " +
                 "\nIf the cell is meant to be blank, enter a zero.");
+        sudokuBoard = generateUserSudokuBoard();
+
     }
 
     public static Board generateUserSudokuBoard(){
         Board newBoard = new Board(SIZE);
 
         for(int row = 0; row < SIZE; row++){
-            for(int col = 0; col < SIZE; col++){
+            for(int col = 0; col < SIZE; col++) {
 
                 int inputValue = kbd.nextInt();
-                newBoard.addCell(inputValue, row, col);
 
-                if(inputValue > SIZE){
+                if (inputValue >= 0 && inputValue <= SIZE) {
+                    newBoard.addCell(inputValue, row, col);
+                } else{
                     //Number is too large, reset everything
                     System.out.println("Invalid number passed in. Restarting.");
-                    row= 0;
+                    row = 0;
                     col = 0;
                     newBoard = new Board(SIZE);
                 }
             }
-        }
+            }
+
         return newBoard;
     }
 
