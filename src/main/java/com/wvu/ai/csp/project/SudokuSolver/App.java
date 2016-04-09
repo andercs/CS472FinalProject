95f30e03f1918eefad10e83e558a5773048d87ac
Connor Anderson <connor.anderson@leidos.com>
2016-04-09 14:03:46 -0400

    Initial Commit

---
+++ src/main/java/com/wvu/ai/csp/project/SudokuSolver/App.java
@@ -0,0 +1,13 @@
+package com.wvu.ai.csp.project.SudokuSolver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
